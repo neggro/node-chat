c7bab101b6b72d7af4c24d3d912b0c899adc9c63
Gustavo <gustavoca245@gmail.com>
2013-10-25 04:02:02 -0200

    change everything, using express and jade templates right now

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express'),
+    routes = require('./routes'),
+    http = require('http'),
+    app = express(),
+    server = app.listen(3000),
+    io = require('socket.io').listen(server);
+
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+
+io.of('/room')
+    .on('connection', function (socket) {
+        console.log('connection!');
+        var joinedRoom = null;
+        socket.on('join room', function (data) {
+            socket.join(data);
+            joinedRoom = data;
+            socket.emit('joined', 'you\'ve joined ' + data);
+            socket.broadcast.to(joinedRoom).send('someone joined room');
+        });
+        socket.on('fromclient', function (data) {
+            if (joinedRoom) {
+                socket.broadcast.to(joinedRoom).send(JSON.stringify(data));
+            } else {
+                socket.send(
+                    'You\'re not joined a room.' +
+                    'Select a room and then push join.'
+                );
+            }
+        });
+        socket.on('leave', function (data) {
+            socket.leave(data);
+            socket.emit('leave room', JSON.stringify({
+                success: true,
+                roomLeaved: data
+            }));
+        });
+    });
+
+    console.log("Express server listening on port 3000");
---
+++ public/javascripts/client.js
@@ -0,0 +1,94 @@
+(function ($) {
+
+    var joined = false,
+        nickName,
+        room,
+        $chatScreen = $('#chat-screen'),
+        $roomText = $('#room-text'),
+        $roomSelect = $('#room-select'),
+        $nickName = $('#nick-name'),
+        selectedRoom,
+        $roomConnectBtn = $('#room-connect-btn'),
+        messageTemplate = '<p><em>{from}: </em>{message}</p>',
+
+        makeRandom = function () {
+            return Math.floor(Math.random() * 99999);
+        },
+
+        bindEvents = function () {
+            $('#nick-name-form').on('submit', function (e) {
+                e.preventDefault();
+                if (!room) {
+                    nickName = $nickName.val();
+                    if (!nickName) {
+                        nickName = 'user_' + makeRandom();
+                    }
+                    room = io.connect('/room');
+                    manageRoom();
+                } else {
+                    room.emit('leave', selectedRoom);
+                }
+            });
+
+            $('#room-join').on('click', function (e) {
+                selectedRoom = $roomSelect.val();
+                room.emit('join room', selectedRoom);
+            });
+
+            $('#room-form').on('submit', function (e) {
+                e.preventDefault();
+                if (joined) {
+                    var msg = $roomText.val();
+                    $roomText.val('');
+                    $chatScreen.append(
+                        messageTemplate.replace('{from}', 'From me')
+                            .replace('{message}', msg)
+                    )[0].scrollTop = 999999;
+                    room.emit('fromclient', {
+                        msg: msg,
+                        from: nickName
+                    });
+                } else {
+                    $chatScreen.append(
+                        messageTemplate.replace('{from}', 'From server')
+                            .replace('{message}', 'Please join a room first.')
+                    )[0].scrollTop = 999999;
+                }
+            });
+        },
+
+        manageRoom = function () {
+            room.on('connect', function () {
+                $('#room-form').css('display', 'block');
+                $chatScreen.append(
+                    messageTemplate.replace('{from}', 'From server')
+                        .replace('{message}', nickName + ' connected.')
+                )[0].scrollTop = 999999;
+                $nickName.val(nickName).attr('readonly', true);
+                $roomConnectBtn.text('Disconnect').addClass('btn-danger');
+            });
+
+            room.on('joined', function (msg) {
+                joined = true;
+                $chatScreen.append(
+                    messageTemplate.replace('{from}', 'From server')
+                        .replace('{message}', msg)
+                )[0].scrollTop = 999999;
+            });
+
+            room.on('message', function (data) {
+                var parsedData = JSON.parse(data);
+                $chatScreen.append(
+                    messageTemplate.replace('{from}', parsedData.from)
+                        .replace('{message}', parsedData.msg)
+                )[0].scrollTop = 999999;
+            });
+
+            room.on('leave room', function (data) {
+                alert(data);
+            });
+        };
+
+    bindEvents();
+
+})(jQuery);
